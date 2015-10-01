5d8202bbb23c435dc682a9e59d948dfc8fc07f44
fasoodev6 <dev6@fasoo.com>
2015-10-01 16:32:01 +0900

    Add Radium LCP

--- SDKLauncher-Android/src/org/readium/sdk/android/launcher/ContainerList.java
+++ SDKLauncher-Android/src/org/readium/sdk/android/launcher/ContainerList.java
@@ -29,31 +29,42 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Stack;
+import java.util.Vector;
 
-import org.readium.sdk.android.EPub3;
 import org.readium.sdk.android.Container;
-import org.readium.sdk.android.launcher.model.BookmarkDatabase;
+import org.readium.sdk.android.CredentialHandler;
+import org.readium.sdk.android.CredentialRequest;
+import org.readium.sdk.android.CredentialRequest.Component;
+import org.readium.sdk.android.EPub3;
+import org.readium.sdk.android.LCP;
 import org.readium.sdk.android.SdkErrorHandler;
+import org.readium.sdk.android.launcher.model.BookmarkDatabase;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 
 /**
  * @author chtian
  * 
  */
-public class ContainerList extends Activity implements SdkErrorHandler {
+@SuppressLint("NewApi") public class ContainerList extends Activity implements SdkErrorHandler {
 	
 	protected abstract class SdkErrorHandlerMessagesCompleted {
 		Intent m_intent = null;
@@ -108,26 +119,9 @@ public class ContainerList extends Activity implements SdkErrorHandler {
 
             	m_SdkErrorHandler_Messages = new Stack<String>();
                 
-                EPub3.setSdkErrorHandler(ContainerList.this);
-                Container container = EPub3.openBook(path);
-                EPub3.setSdkErrorHandler(null);
-                
-                ContainerHolder.getInstance().put(container.getNativePtr(), container);
-
-                Intent intent = new Intent(getApplicationContext(), BookDataActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra(Constants.BOOK_NAME, bookName);
-                intent.putExtra(Constants.CONTAINER_ID, container.getNativePtr());
-                
-                SdkErrorHandlerMessagesCompleted callback = new SdkErrorHandlerMessagesCompleted(intent) {
-					@Override
-					public void once() {
-		                startActivity(m_intent);
-					}
-                };
-
-                // async!
-                popSdkErrorHandlerMessage(context, callback); 
+            	//Openbook run on thread
+            	OpenBookAsyncTask openBook = new OpenBookAsyncTask();
+                openBook.execute(path, bookName);
             }
         });
         
@@ -245,5 +239,124 @@ public class ContainerList extends Activity implements SdkErrorHandler {
 
 		});
         return list;
+    }
+
+    public class OpenBookAsyncTask extends AsyncTask<String, Vector<Component>, Container> implements CredentialHandler{
+ 
+    	protected String bookName;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+         
+        @Override
+        protected Container doInBackground(String...params) {
+
+        	bookName = params[1];
+        	EPub3.setSdkErrorHandler(ContainerList.this);
+        	LCP.InitializeLCP();
+        	LCP.setCredentialHandler(this);
+        	Container container = EPub3.openBook(params[0]);
+        	EPub3.setSdkErrorHandler(null);
+        	LCP.setCredentialHandler(null);
+        	return container;
+        }        
+        @Override
+        protected void onPostExecute(Container container) {
+        	if(container == null)
+        		return;
+        	ContainerHolder.getInstance().put(container.getNativePtr(), container);
+
+        	Intent intent = new Intent(getApplicationContext(), BookDataActivity.class);
+        	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        	intent.putExtra(Constants.BOOK_NAME, bookName);
+        	intent.putExtra(Constants.CONTAINER_ID, container.getNativePtr());
+        	
+        	SdkErrorHandlerMessagesCompleted callback = new SdkErrorHandlerMessagesCompleted(intent) {
+				@Override
+				public void once() {
+	                startActivity(m_intent);
+				}
+          };
+
+          popSdkErrorHandlerMessage(context, callback);
+        }
+         
+        @Override
+        protected void onCancelled() {
+        	super.onCancelled();
+        }
+
+        public AlertDialog alertD;
+        
+        @Override
+        protected void onProgressUpdate(Vector<Component>... param) {
+        	Vector<Component> components = param[0];
+
+	    	Log.i("OpenBookAsyncTask", "onProgressUpdate : " + components.size());
+        	LinearLayout layout = new LinearLayout(context);
+        	layout.setOrientation(LinearLayout.VERTICAL);
+        	
+        	AlertDialog.Builder alertBuilder  = new AlertDialog.Builder(context);
+			alertBuilder.setTitle(components.get(0).m_title);
+			alertBuilder.setMessage(components.get(1).m_title);
+			for(int i=2; i<components.size(); i++)
+			{
+				Component component = components.get(i);
+				CredentialRequest.Type type = component.m_type;
+				if(type == CredentialRequest.Type.Message){
+					
+				}else if(type == CredentialRequest.Type.TextInput){
+					EditText input = new EditText(context);
+					layout.addView(input);
+				}else if(type == CredentialRequest.Type.MaskedInput){
+					EditText input = new EditText(context);
+					input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD|InputType.TYPE_CLASS_TEXT);
+					input.setTag(component);
+					layout.addView(input);
+					
+					Button button = new Button(context);
+					button.setText("ok");
+					button.setTag(input);
+					button.setOnClickListener(new OnClickListener() {
+						@Override
+						public void onClick(View view) {
+							alertD.dismiss();
+							EditText input = (EditText)view.getTag();
+							Component component = (Component)input.getTag();
+							LCP.CredentialResult(component.m_index, input.getText().toString());
+						}
+			    	});
+					layout.addView(button);
+				}else if(type == CredentialRequest.Type.Button){
+	                if(components.get(i).m_title.equals("Forgot password"))
+	                	continue;
+
+					Button button = new Button(context);
+					button.setText(components.get(i).m_title);
+					button.setTag(component);
+					button.setOnClickListener(new OnClickListener() {
+						@Override
+						public void onClick(View view) {
+							alertD.dismiss();
+							Component component = (Component)view.getTag();
+							LCP.CredentialResult(component.m_index, "");
+						}
+			    	});
+					layout.addView(button);
+				}
+			}
+			alertBuilder.setView(layout);
+			alertBuilder.setCancelable(false);
+			alertD = alertBuilder.create();
+			alertD.show();
+        }
+        
+		@Override
+		public boolean handleCredential(Vector<Component> components) {
+	    	Log.i("OpenBookAsyncTask", "on handleCredential");
+			publishProgress(components);
+			return false;
+		}
     }
 }
